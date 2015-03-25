6a3342b1477fd75cd8e67cf136bf4bace4a3a93c
Simonov Dmitry <demalf@gmail.com>
2015-03-25 23:13:21 +0300

    fast

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 
 from setuptools import setup
 
-setup(name='model_render',
+setup(name='django-model-render',
     version='0.1',
     description='Django models extension that allows define default model templates',
     author='a1fred',
     author_email='demalf@gmail.com',
     license='MIT',
+    url='https://github.com/a1fred/django-model-render',
     packages=['model_render'],
     zip_safe=False,
     install_requires = [
